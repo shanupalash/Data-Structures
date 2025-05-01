4a48401776ff91a6d720d7f7982895164d03edab
palash sahu <73164304+shanupalash@users.noreply.github.com>
2025-05-01 15:29:04 +0530

    Create MergeSortedArray.java

---
+++ MergeSortedArray.java
@@ -0,0 +1,40 @@
+class MergeSortedArray {
+    
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+       
+         int i = m - 1;     
+        int j = n - 1;     
+        int k = m + n - 1;  
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+            
+             for(int ele : nums1){
+            System.out.println(ele);
+        }
+    }
+        
+       
+    public static void main(String[] args) {
+       
+      int[] nums1 = {1,2,3,5,9,11};
+      int m = 6;
+      int[] nums2 = {2,5,6};
+      int n = 3;
+      
+      merge(nums1,m,nums2,n);
+      
+      
+      
+    }
+
+ }
